7fc2614ab3b77f0ed7c091892c4f7d35ec69b212
bugra9 <bugra9@gmail.com>
2017-06-03 17:10:51 +0300

    travis denemesi 2

--- push.sh
+++ push.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-build() {
-  echo "deneme" > a.txt
-}
-
-upload_files() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-  git add .
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-  git push https://${GH_TOKEN}@github.com/bugra9/test.git HEAD:master
-}
-
-build
-upload_files
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git add .
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git push https://${GH_TOKEN}@github.com/bugra9/test.git HEAD:master
