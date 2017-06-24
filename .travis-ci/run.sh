5ee8be30fbafe05fdb4c9e484079cc2d329d827a
Bugra <bugra9@users.noreply.github.com>
2017-06-24 14:13:07 +0300

    Update run.sh

--- .travis-ci/run.sh
+++ .travis-ci/run.sh
@@ -1,8 +1,11 @@
+cd .travis-ci
 for VAR in $(find . -maxdepth 1 -type d -name [^\.]\* | sed 's:^\./::')
 do
+	echo $VAR
 	bash $VAR/run.sh
 done
 
+cd ..
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git add .
