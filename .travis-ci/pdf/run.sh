05f2f8b67aa29647e41bb16f6e2910d507732a01
Bugra <bugra9@users.noreply.github.com>
2017-06-24 14:05:56 +0300

    Update and rename control.sh to run.sh

---
+++ .travis-ci/pdf/run.sh
@@ -0,0 +1,19 @@
+RUN=0
+for VAR in $(git log --name-only -n 1 HEAD~1..HEAD)
+do
+	if [[ $VAR == _posts/*.md ]]; then
+    RUN=1
+  fi
+done
+
+if [[ $RUN == 1 ]]; then
+  bash ./install.sh
+  
+  for VAR in $(git log --name-only -n 1 HEAD~1..HEAD)
+  do
+    if [[ $VAR == _posts/*.md ]]; then
+      bash ./createPdf.sh $VAR
+    fi
+  done
+  
+fi
