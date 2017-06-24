08c4567dde78a7122230cb803dc90bafde77edd3
Bugra <bugra9@users.noreply.github.com>
2017-06-24 13:42:14 +0300

    Update push.sh

--- .travis-ci/push.sh
+++ .travis-ci/push.sh
@@ -1,6 +1,6 @@
 for VAR in $(find . -maxdepth 1 -type d -name [^\.]\* | sed 's:^\./::')
 do
-	echo ${VAR}
+	bash $VAR/run.sh
 done
 
 git push https://${GH_TOKEN}@github.com/bugra9/test.git HEAD:master
