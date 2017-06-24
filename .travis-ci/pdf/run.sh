97c9351f5fcb6ef20ff8ae028dcc8ba0abbee0b9
Bugra <bugra9@users.noreply.github.com>
2017-06-24 14:24:19 +0300

    Update run.sh

--- .travis-ci/pdf/run.sh
+++ .travis-ci/pdf/run.sh
@@ -1,8 +1,10 @@
 RUN=0
 for VAR in $(git log --name-only -n 1 HEAD~1..HEAD)
 do
-	if [[ $VAR == _posts/*.md ]]; then
+  if [[ $VAR == _posts/*.md ]]; then
     RUN=1
+  else
+    echo "No new post found."
   fi
 done
 
