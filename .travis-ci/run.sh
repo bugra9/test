22f61da80f07259c0b0835e8873124e505a2bee8
Bugra <bugra9@users.noreply.github.com>
2017-06-24 14:07:42 +0300

    Update run.sh

--- .travis-ci/run.sh
+++ .travis-ci/run.sh
@@ -3,4 +3,8 @@ do
 	bash $VAR/run.sh
 done
 
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git add .
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
 git push https://${GH_TOKEN}@github.com/bugra9/test.git HEAD:master
