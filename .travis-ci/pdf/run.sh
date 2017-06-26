2d443206fbb2d3b2608f9287d910d6f7417bb306
Bugra <bugra9@users.noreply.github.com>
2017-06-26 15:49:43 +0200

    Update run.sh

--- .travis-ci/pdf/run.sh
+++ .travis-ci/pdf/run.sh
@@ -1,5 +1,5 @@
 RUN=0
-for VAR in $(git log --name-only -n 1 HEAD~1..HEAD)
+for VAR in $(git diff-tree --no-commit-id --name-only -r HEAD~1..HEAD)
 do
   if [[ $VAR == _posts/*.md ]]; then
     RUN=1
@@ -9,7 +9,7 @@ done
 if [[ $RUN == 1 ]]; then
   bash ./install.sh
   
-  for VAR in $(git log --name-only -n 1 HEAD~1..HEAD)
+  for VAR in $(git diff-tree --no-commit-id --name-only -r HEAD~1..HEAD)
   do
     if [[ $VAR == _posts/*.md ]]; then
       bash ./createPdf.sh $VAR
