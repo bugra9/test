40e7515cb6e0c69413f92df16cf63e6bf238e815
Bugra <bugra9@users.noreply.github.com>
2017-06-26 15:01:52 +0200

    Update createThumbnail.sh

--- .travis-ci/thumbnail/createThumbnail.sh
+++ .travis-ci/thumbnail/createThumbnail.sh
@@ -4,7 +4,7 @@ cd ..
 AA=$(echo $1 | rev)
 DEST_CARD=$(echo ${AA/\//_drac\/} | rev)
 DEST_ITEM=$(echo ${AA/\//_meti\/} | rev)
-convert -resize 370X220! $1 DEST_CARD
-convert -resize 80 $1 DEST_ITEM
+convert -resize 370X220! $1 $DEST_CARD
+convert -resize 80 $1 $DEST_ITEM
 
 cd .travis-ci/thumbnail
