56fb24eb7121d536d9b683d9e9b95faec7636628
Bugra <bugra9@users.noreply.github.com>
2017-06-26 15:50:18 +0200

    Update run.sh

--- .travis-ci/thumbnail/run.sh
+++ .travis-ci/thumbnail/run.sh
@@ -1,5 +1,5 @@
 RUN=0
-for VAR in $(git log --name-only -n 1 HEAD~1..HEAD)
+for VAR in $(git diff-tree --no-commit-id --name-only -r HEAD~1..HEAD)
 do
   if [[ $VAR == assets/post/*/*[.jpg.jpeg.JPG.JPEG.gif.GIF.png.PNG] && $(echo $VAR | grep -e "/card_" -e "/item_") == "" ]]; then
     RUN=1
@@ -7,7 +7,7 @@ do
 done
 
 if [[ $RUN == 1 ]]; then
-  for VAR in $(git log --name-only -n 1 HEAD~1..HEAD)
+  for VAR in $(git diff-tree --no-commit-id --name-only -r HEAD~1..HEAD)
   do
     if [[ $VAR == assets/post/*/*[.jpg.jpeg.JPG.JPEG.gif.GIF.png.PNG] && $(echo $VAR | grep -e "/card_" -e "/item_") == "" ]]; then
       bash ./createThumbnail.sh $VAR
