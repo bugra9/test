52f7afcc86d852f313a620d9f920c0b4b0b34c66
Bugra <bugra9@users.noreply.github.com>
2017-07-02 13:36:03 +0300

    Update run.sh

--- .travis-ci/run.sh
+++ .travis-ci/run.sh
@@ -12,5 +12,5 @@ cd ..
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git add .
-git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
+git commit --message "[BOT] Makale için otomatik ayarlamalar yapıldı. [skip ci]"
 git push https://${GH_TOKEN}@github.com/bugra9/test.git HEAD:master
